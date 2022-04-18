450100b208dcf9ad79ebfc3531dcf12fce755ab2
Josh Lalonde <mori.ryuuji3@gmail.com>
2022-04-18 19:38:44 -0300

    multi results

--- src/stories/Calculator.stories.jsx
+++ src/stories/Calculator.stories.jsx
@@ -49,3 +49,42 @@ Simple.args = {
         },
     ],
 }
+
+export const MultiResults = Template.bind({})
+MultiResults.args = {
+    initialRows: [
+        {
+            name: 'a',
+            label: 'Initial value',
+            value: 5,
+        },
+        {
+            name: 'c',
+            label: 'Multiply',
+            operator: '*',
+            value: 2,
+        },
+        {
+            name: 'result1',
+            label: 'Result 1',
+            operator: '=',
+        },
+        {
+            name: 'd',
+            label: 'Divide',
+            operator: '/',
+            value: 2,
+        },
+        {
+            name: 'e',
+            label: 'Subtract',
+            operator: '-',
+            value: 2,
+        },
+        {
+            name: 'result2',
+            label: 'Result 2',
+            operator: '=',
+        },
+    ],
+}
